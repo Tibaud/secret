699f090817573c322b7e07ed9afc8795d2028d43
Tibaud <et@sumatra.fr>
2018-02-05 23:28:52 +0100

    V1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  # on check si le user est valide
+  before_action :require_valid_user!
+
+  def current_user
+    if !session[:user_id].blank?
+      @user ||= User.find(session[:user_id])
+    end
+  end
+  # si user=nil, on redirige vers la page de login
+  def require_valid_user!
+    if current_user.nil?
+      flash[:error] = 'Vous devez être connecté pour accéder à cette page!'
+      redirect_to login_path
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,26 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'dashboard/show'
+
+  # on met le dashboard en view par défaut
+  root 'dashboard#show'
+
+  get 'users/new'
+  # on détermine les URL login et logout
+  get 'login', to: 'sessions#new'
+  get 'logout', to: 'sessions#destroy'
+
+  resources :sessions, only: [:create]
+  resources :users, only: [:new, :create]
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
